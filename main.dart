aba5171996f4c0a038a6b63e1ce6c24a7b8bd781
Adwaith Krishna <adwaithambiyil@gmail.com>
2024-03-28 15:43:49 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'pages/signup.dart';
+import 'pages/resetpass.dart'; 
+import 'pages/role.dart';
+import 'pages/login.dart';
+import 'pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Login(),
+        '/signup': (context) => Signup(),
+        '/role': (context) => Role(),
+        '/reset': (context) => ResetPasswordScreen(), // Ensure ResetPasswordScreen is defined
+        '/home': (context) => Homepage(),
+      },
+    );
+  }
+}
